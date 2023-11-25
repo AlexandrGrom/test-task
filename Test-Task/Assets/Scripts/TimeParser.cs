447fcb6a768d76f7ec41557210505ba8849b2c8a
AlexandrGrom <68745248+AlexandrGrom@users.noreply.github.com>
2023-11-25 17:29:18 +0200

    created simple scene

--- Test-Task/Assets/Scripts/TimeParser.cs
+++ Test-Task/Assets/Scripts/TimeParser.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 
 public class TimeParser : MonoBehaviour
 {
-    [SerializeField] private Transform t;
-    private void Awake()
+    [SerializeField] private Transform arrowH;
+    [SerializeField] private Transform arrowM;
+    [SerializeField] private Transform arrowS;
+
+    private void Update()
     {
         TimeZoneInfo gmtTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
         DateTime gmtTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtTimeZone);
         
-        Debug.Log("Current Time in GMT: " + gmtTime.Hour);
-        t.rotation = Quaternion.Euler(0f, 360f / gmtTime.Hour, 0f);
+        float rotationAngleX = -gmtTime.Hour % 12 * (360f / 12f);
+        arrowH.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
+
+        rotationAngleX = -gmtTime.Minute * (360f / 60f);
+        arrowM.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
+        
+        rotationAngleX = -gmtTime.Second * (360f / 60f);
+        arrowS.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
     }
 }
