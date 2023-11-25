d89974fd46ceb8362d0b5c4b0a228edebf31bc28
AlexandrGrom <68745248+AlexandrGrom@users.noreply.github.com>
2023-11-25 18:13:15 +0200

    maked simple build for android

--- Test-Task/Assets/Scripts/TimeParser.cs
+++ Test-Task/Assets/Scripts/TimeParser.cs
@@ -6,19 +6,18 @@ public class TimeParser : MonoBehaviour
     [SerializeField] private Transform arrowH;
     [SerializeField] private Transform arrowM;
     [SerializeField] private Transform arrowS;
-
+    
+    
     private void Update()
     {
-        TimeZoneInfo gmtTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-        DateTime gmtTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtTimeZone);
-        
-        float rotationAngleX = -gmtTime.Hour % 12 * (360f / 12f);
-        arrowH.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
+        DateTime gmtTime = DateTime.UtcNow;
 
-        rotationAngleX = -gmtTime.Minute * (360f / 60f);
-        arrowM.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
-        
-        rotationAngleX = -gmtTime.Second * (360f / 60f);
-        arrowS.localRotation = Quaternion.Euler(rotationAngleX, 0f, 0f);
+        float rotationAngleXHour = -gmtTime.Hour % 12 * (360f / 12f);
+        float rotationAngleXMinute = -gmtTime.Minute * (360f / 60f);
+        float rotationAngleXSecond = -gmtTime.Second * (360f / 60f);
+
+        arrowH.localRotation = Quaternion.Euler(rotationAngleXHour, 0f, 0f);
+        arrowM.localRotation = Quaternion.Euler(rotationAngleXMinute, 0f, 0f);
+        arrowS.localRotation = Quaternion.Euler(rotationAngleXSecond, 0f, 0f);
     }
 }
